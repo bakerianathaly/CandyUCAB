015007f528ab61a5ae3633fd97980a9dc7c5f679
Rommel Liendo <cloudyrl1@gmail.com>
2018-06-16 14:26:50 -1200

    Carrito
    agregado el control de carrito y la vista principal

---
+++ resources/views/carrito.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.index')
+@section('title','Listar producto')
+@section('content')
+    <div class="container-fluid">
+        <div class="m-2">
+            <div class=" ">
+                <h2 id="registro-letras">Productos</h2>
+            </div>
+        </div>
+    </div>
+      @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+        <div class="container">
+    <br />
+    @if (\Session::has('success'))
+      <div class="alert alert-success">
+        <p>{{ \Session::get('success') }}</p>
+      </div><br />
+     @endif
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Nombre</th>
+        <th>Puntuacion</th>
+        <th>Precio</th>
+        <th colspan="2">Action</th>
+      </tr>
+    </thead>
+    {{-- <tbody>
+      @foreach($productos as $producto)
+      <tr>
+        <td>{{$producto->pro_id}}</td>
+        <td>{{$producto->pro_nombre}}</td>
+        <td>{{$producto->pro_puntuacion}}</td>
+        <td>{{$producto->pro_relleno}}</td>
+        <td>{{$producto->pro_textura}}</td>
+        <td>{{$producto->pro_descripcion}}</td>
+
+        <td><a  href="{{action('ProductosController@edit', $producto->pro_id)}}" class="btn btn-warning">Editar</a></td>
+        <input name="_method" type="hidden" value="PATCH">
+        <td>
+          <form action="{{action('ProductosController@destroy', $producto->pro_id)}}" method="post">
+            {{csrf_field()}}
+            <input name="_method" type="hidden" value="DELETE">
+            <button class="btn btn-danger" type="submit">Eliminar</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </tbody> --}}
+  </table>
+       <td><a href="{{action('ProductosController@create')}}" class="btn centrar_boton">Agregar producto</a></td>
+  </div>
+    {!! Form::close() !!}
+@endsection
