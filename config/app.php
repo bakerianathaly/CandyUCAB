49a24467f3ba532fe6be7589d3c67556daeac3f7
bakerianathaly <bakerianathaly@gmail.com>
2018-06-20 01:34:58 -0400

    Solucionado errores con EXCEL

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -212,7 +212,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
